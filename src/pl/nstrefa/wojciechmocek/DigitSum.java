6465cc571b2a705e036e2323edc10532c20b3c33
Sieciech <wojciech.mocek@o2.pl>
2019-05-12 15:18:31 +0200

    added new method sumFirstAndLast

--- src/pl/nstrefa/wojciechmocek/DigitSum.java
+++ src/pl/nstrefa/wojciechmocek/DigitSum.java
@@ -17,4 +17,25 @@ class DigitSum {
         return digitSum;
 
     }
+
+    public int sumFirstAndLast(int number) {
+        if (number < 10 && number > -10) {
+            return number;
+        }
+
+        int digitSum = 0;
+        int divider = number>=0 ? 10 : -10;
+        int digit = number % divider;
+        number /= 10;
+        digitSum += digit;
+
+        while (number != 0) {
+            digit = number % divider;
+            number /= 10;
+        }
+        digitSum += digit;
+
+        return digitSum;
+
+    }
 }
--- src/pl/nstrefa/wojciechmocek/Test.java
+++ src/pl/nstrefa/wojciechmocek/Test.java
@@ -5,6 +5,12 @@ package pl.nstrefa.wojciechmocek;
  */
 public class Test {
     public static void main(String[] args) {
+        testDigitSum();
+        testDigitSumFirstAndLast();
+
+    }
+
+    private static void testDigitSum() {
         System.out.println("Testing DigitSum:");
 
         DigitSum digitSum = new DigitSum();
@@ -19,10 +25,31 @@ public class Test {
         assert -2 == digitSum.sum(-2);
         assert -3 == digitSum.sum(-21);
 
-        System.out.println("All assertions for " + digitSum.getClass().getName() + " are met");
+        System.out.println("All assertions for DigitSum are met");
 
         System.out.println("\n----------------------\n");
+    }
 
+    private static void testDigitSumFirstAndLast() {
+        System.out.println("Testing DigitSumFirstAndLast:");
+
+        DigitSum digitSum = new DigitSum();
+
+        assert 1 == digitSum.sumFirstAndLast(1);
+        assert 2 == digitSum.sumFirstAndLast(2);
+        assert 3 == digitSum.sumFirstAndLast(21);
+        assert 7 == digitSum.sumFirstAndLast(215);
+        assert 11 == digitSum.sumFirstAndLast(2159);
+        assert 0 == digitSum.sumFirstAndLast(0);
+        assert -1 == digitSum.sumFirstAndLast(-1);
+        assert -2 == digitSum.sumFirstAndLast(-2);
+        assert -3 == digitSum.sumFirstAndLast(-21);
+        assert -7 == digitSum.sumFirstAndLast(-215);
+        assert -11 == digitSum.sumFirstAndLast(-2159);
+
+        System.out.println("All assertions for DigitSumFirstAndLast are met");
+
+        System.out.println("\n----------------------\n");
     }
 
 }
