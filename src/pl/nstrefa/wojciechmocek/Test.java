b2a920b7fadbc406ebe5ef1aa85720d529f11fbd
Sieciech <wojciech.mocek@o2.pl>
2019-05-13 13:45:06 +0200

    reorganized files

--- src/pl/nstrefa/wojciechmocek/Test.java
+++ src/pl/nstrefa/wojciechmocek/Test.java
@@ -7,8 +7,8 @@ public class Test {
     public static void main(String[] args) {
         testDigitSum();
         testDigitSumFirstAndLast();
-        testIfForeachEvaluatesMethodEachTime();
-        testIfForLoopEvaluatesMethodEachTime();
+        ForLoopTest.testIfForeachEvaluatesMethodEachTime();
+        ForLoopTest.testIfForLoopEvaluatesMethodEachTime();
     }
 
     private static void testDigitSum() {
@@ -52,35 +52,4 @@ public class Test {
 
         System.out.println("\n----------------------\n");
     }
-
-    private static void testIfForeachEvaluatesMethodEachTime() {
-        for (int i : getNumbers()) {
-//            System.out.println(i);
-        }
-//        System.out.println("getNumbers called " + numberOfTimesCalledGetNumbers + " times");
-        assert 1 == numberOfTimesCalledGetNumbers;
-        numberOfTimesCalledGetNumbers = 0;
-    }
-
-    private static int numberOfTimesCalledGetNumbers = 0;
-    private static int[] getNumbers() {
-        ++numberOfTimesCalledGetNumbers;
-        return new int[]{1,2,3,4,5};
-    }
-
-    private static int numberOfTimesCalledGetSize = 0;
-    private static void testIfForLoopEvaluatesMethodEachTime() {
-        for (int i=0; i<getSize(); ++i) {
-//            System.out.println(i);
-        }
-//        System.out.println("getSize called " + numberOfTimesCalledGetSize + " times");
-        assert 6 == numberOfTimesCalledGetSize;
-        numberOfTimesCalledGetSize = 0;
-    }
-
-    private static int getSize() {
-        ++numberOfTimesCalledGetSize;
-        return 5;
-    }
-
 }
