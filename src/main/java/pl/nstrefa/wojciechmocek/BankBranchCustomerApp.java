600fb5253484419a09c61c6afbb98de2a2e463f6
Sieciech <wojciech.mocek@o2.pl>
2019-05-16 15:00:56 +0200

    Used bank library in BankBranchCustomerApp

--- src/main/java/pl/nstrefa/wojciechmocek/BankBranchCustomerApp.java
+++ src/main/java/pl/nstrefa/wojciechmocek/BankBranchCustomerApp.java
@@ -1,9 +1,38 @@
 package pl.nstrefa.wojciechmocek;
 
-import pl.nstrefa.wojciechmocek.bank.BankInMemory;
+import pl.nstrefa.wojciechmocek.bank.Bank;
+import pl.nstrefa.wojciechmocek.bank.BankFactory;
+import pl.nstrefa.wojciechmocek.bank.BranchCustomerNotExistsException;
+import pl.nstrefa.wojciechmocek.bank.CustomerAccountAlreadyCreatedException;
 
 public class BankBranchCustomerApp {
     public static void main(String[] args) {
-        BankInMemory bank = new BankInMemory();
+
+        Bank bank = new BankFactory().make();
+
+        String branchName = args[0];
+        bank.createBranch(branchName);
+
+        String customer = args[1];
+
+        double[] transactions = new double[args.length - 2];
+        for (int i = 0, argIndex = 2; argIndex < args.length; ++i, ++argIndex) {
+            transactions[i] = Double.parseDouble(args[argIndex]);
+        }
+
+        try {
+            bank.createAccount(branchName, customer, transactions[0]);
+            for (int i = 1; i < transactions.length; ++i) {
+                bank.addTransaction(branchName, customer, transactions[i]);
+            }
+
+            System.out.printf(
+                "Customer %s's transactions are: %s\n",
+                customer,
+                bank.getCustomerTransactions(branchName, customer)
+            );
+        } catch (CustomerAccountAlreadyCreatedException|BranchCustomerNotExistsException e) {
+            e.printStackTrace();
+        }
     }
 }
