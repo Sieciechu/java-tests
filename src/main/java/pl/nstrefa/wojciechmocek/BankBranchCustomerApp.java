7d8024bcd8d89a21c510f416c54b898509136dda
Sieciech <wojciech.mocek@o2.pl>
2019-05-17 20:11:29 +0200

    Removed not used methods

--- src/main/java/pl/nstrefa/wojciechmocek/BankBranchCustomerApp.java
+++ src/main/java/pl/nstrefa/wojciechmocek/BankBranchCustomerApp.java
@@ -2,7 +2,6 @@ package pl.nstrefa.wojciechmocek;
 
 import pl.nstrefa.wojciechmocek.bank.Bank;
 import pl.nstrefa.wojciechmocek.bank.BankFactory;
-import pl.nstrefa.wojciechmocek.bank.Exception;
 
 public class BankBranchCustomerApp {
 
@@ -16,35 +15,5 @@ public class BankBranchCustomerApp {
         Bank bank = new BankFactory().make();
         var app = new BankBranchCustomerApp(new CliController(bank));
         app.controller.run();
-
-    }
-
-    private static void runUsingArgs(String[] args) {
-        Bank bank = new BankFactory().make();
-
-        String branchName = args[0];
-        bank.createBranch(branchName);
-
-        String customer = args[1];
-
-        double[] transactions = new double[args.length - 2];
-        for (int i = 0, argIndex = 2; argIndex < args.length; ++i, ++argIndex) {
-            transactions[i] = Double.parseDouble(args[argIndex]);
-        }
-
-        try {
-            bank.createAccount(branchName, customer, transactions[0]);
-            for (int i = 1; i < transactions.length; ++i) {
-                bank.addTransaction(branchName, customer, transactions[i]);
-            }
-
-            System.out.printf(
-                "Customer %s's transactions are: %s\n",
-                customer,
-                bank.getCustomerTransactions(branchName, customer)
-            );
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
     }
 }
--- src/main/java/pl/nstrefa/wojciechmocek/bank/Exception.java
+++ src/main/java/pl/nstrefa/wojciechmocek/bank/Exception.java
@@ -7,16 +7,4 @@ public class Exception extends java.lang.Exception {
     public Exception(String message) {
         super(message);
     }
-
-    public Exception(String message, Throwable cause) {
-        super(message, cause);
-    }
-
-    public Exception(Throwable cause) {
-        super(cause);
-    }
-
-    public Exception(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
-        super(message, cause, enableSuppression, writableStackTrace);
-    }
 }
